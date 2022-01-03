0fd66534d9e10937c468b8536548bee46c26e222
jedavis82 <davis.jeremy.e@gmail.com>
2022-01-03 16:45:54 -0600

    Adding visualization for the HOF output
    For the paper, creating images for the HOF output.

---
+++ visualizations/generate_hof_output.py
@@ -0,0 +1,91 @@
+"""
+For the paper, generate results for hof for the input images and store in CSV files.
+"""
+import pandas as pd
+import cv2
+import numpy as np
+import matlab.engine
+import json
+import os
+from level_one_utils.hof_giou import compute_hof_display
+
+ROOT_DIR = '../input/coco_images/'
+IMAGE_PATHS = ['000000000036.jpg', '000000000086.jpg', '000000000459.jpg', '000000000634.jpg', '000000000962.jpg',
+               '000000001655.jpg']
+HIST_OUTPUT_DIR = './hist_vis/'
+if not os.path.exists(HIST_OUTPUT_DIR):
+    os.makedirs(HIST_OUTPUT_DIR)
+
+PERSON_L2_SUMMARIES_FILE = '../output/person_level_two_summaries.csv'
+
+
+def main():
+    # Start a matlab instance
+    eng = matlab.engine.start_matlab()
+    eng.cd('../level_one_utils')
+
+    person_df = pd.read_csv(PERSON_L2_SUMMARIES_FILE, encoding='utf-8', engine='python')
+
+    person_df_filtered = person_df[person_df['relative_path'].isin(IMAGE_PATHS)]
+
+    for idx, row in person_df_filtered.iterrows():
+        if row['person_level_two_summary'] == 'Negative Interaction':
+            continue  # No need to compute the negative interactions
+        rel_path = row['relative_path']
+        arg_label = row['arg_label']
+        arg_box = json.loads(row['arg_bounding_box'])
+        ref_label = row['ref_label']
+        ref_box = json.loads(row['ref_bounding_box'])
+        img_path = ROOT_DIR + rel_path
+        orig_img = cv2.imread(img_path, cv2.IMREAD_ANYCOLOR)
+        img_width = orig_img.shape[1]
+        img_height = orig_img.shape[0]
+        matlab_args = {'img_width': img_width,
+                       'img_height': img_height,
+                       'ref_bounding_box': ref_box,
+                       'arg_bounding_box': arg_box
+                       }
+        f0, f2, hyb = compute_hof_display(eng, matlab_args)
+
+        out_df = pd.DataFrame(columns=['degrees', 'f0_magnitude', 'f2_magnitude', 'hyb_magnitude'])
+        out_df['degrees'] = np.arange(361)
+
+        f0_magnitudes = f0.flatten().tolist()
+        f2_magnitudes = f2.flatten().tolist()
+        hyb_magnitudes = hyb.flatten().tolist()
+        for i in range(361):
+            out_df.loc[out_df['degrees'] == i, 'f0_magnitude'] = f0_magnitudes[i]
+            out_df.loc[out_df['degrees'] == i, 'f2_magnitude'] = f2_magnitudes[i]
+            out_df.loc[out_df['degrees'] == i, 'hyb_magnitude'] = hyb_magnitudes[i]
+
+        out_file = HIST_OUTPUT_DIR + arg_label + '_' + ref_label + '_hists.csv'
+        out_df.to_csv(out_file, encoding='utf-8', header=True, index=False)
+
+        # Plot the resultant histograms
+        # hist_file = HIST_OUTPUT_DIR + arg_label + '_' + ref_label
+        # plt.bar(np.arange(361), f0)
+        # plt.xticks(np.arange(0, 361, 45))
+        # plt.savefig(hist_file + '_f0.png')
+        # plt.show()
+        #
+        # plt.bar(np.arange(361), f2)
+        # plt.xticks(np.arange(0, 361, 45))
+        # plt.savefig(hist_file + '_f2.png')
+        # plt.show()
+        #
+        # plt.bar(np.arange(361), hyb)
+        # plt.xticks(np.arange(0, 361, 45))
+        # plt.savefig(hist_file + '_hyb.png')
+        # plt.show()
+        #
+        # print('Finished creating histogram. Drawing Mask Image')
+        #
+        # blank_img = np.zeros((img_height, img_width, 3), np.uint8)
+        # cv2.rectangle(blank_img, (arg_box[0], arg_box[1]), (arg_box[2], arg_box[3]), (255, 255, 255), -1)
+        # cv2.rectangle(blank_img, (ref_box[0], ref_box[1]), (ref_box[2], ref_box[3]), (128, 128, 128), -1)
+        # img_file = hist_file + '_bounding_boxes.jpg'
+        # cv2.imwrite(img_file, blank_img)
+
+
+if __name__ == '__main__':
+    main()
--- visualizations/visualize_hof.py
+++ visualizations/visualize_hof.py
@@ -1,75 +1,54 @@
 """
-For the paper, visualize the histogram results for an image.
+For the paper, visualize the HOF algorithm output.
+Will do these one at a time so that I can place the legends correctly
 """
 import pandas as pd
-import cv2
 import numpy as np
-import matlab.engine
-import json
-import os
 import matplotlib.pyplot as plt
-from level_one_utils.hof_giou import compute_hof_display
+from pathlib import Path
 
-# Sampling a few image paths for the paper to visualize HOF results with.
-ROOT_DIR = '../input/coco_images/'
-IMAGE_PATHS = ['000000000036.jpg', '000000000086.jpg', '000000000459.jpg']
-HIST_OUTPUT_DIR = './hist_vis/'
-if not os.path.exists(HIST_OUTPUT_DIR):
-    os.makedirs(HIST_OUTPUT_DIR)
-
-PERSON_L2_SUMMARIES_FILE = '../output/person_level_two_summaries.csv'
+HIST_DIR = './hist_vis/'
+PLOTTING_FILE = './hist_vis/person_1_umbrella_1_hists.csv'
+IMG_OUTPUT_FILE = './hist_vis/umbrella_hof.png'
 
 
 def main():
-    # Start a matlab instance
-    eng = matlab.engine.start_matlab()
-    eng.cd('../level_one_utils')
+    # filepath = Path(HIST_DIR).glob('**/*')
+    # hist_files = [x for x in filepath if x.is_file()]
+    
+    # The membership function lines to plot
+    left_x = [120, 180, 240]
+    left_y = [0, 100, 0]
+    below_x = [210, 270, 330]
+    below_y = [0, 100, 0]
+    above_x = [30, 90, 150]
+    above_y = [0, 100, 0]
+    right1_x = [0, 30, 60]
+    right1_y = [0, 100, 0]
+    right2_x = [300, 330, 360]
+    right2_y = [0, 100, 0]
 
-    person_df = pd.read_csv(PERSON_L2_SUMMARIES_FILE, encoding='utf-8', engine='python')
+    df = pd.read_csv(PLOTTING_FILE, encoding='utf-8', engine='python')
+    magnitude = list(df['hyb_magnitude'])
+    fig, ax = plt.subplots(dpi=300)
+    ax.plot(left_x, left_y, color='red', label='left', alpha=0.5)
+    plt.fill_between(left_x, left_y, color='red', alpha=0.5)
+    ax.plot(below_x, below_y, color='green', label='below', alpha=0.5)
+    plt.fill_between(below_x, below_y, color='green', alpha=0.5)
+    ax.plot(above_x, above_y, color='blueviolet', label='above', alpha=0.5)
+    plt.fill_between(above_x, above_y, color='blueviolet', alpha=0.5)
+    ax.plot(right1_x, right1_y, color='darkorange', label='right', alpha=0.5)
+    plt.fill_between(right1_x, right1_y, color='darkorange', alpha=0.5)
+    ax.plot(right2_x, right2_y, color='darkorange', alpha=0.5)
+    plt.fill_between(right2_x, right2_y, color='darkorange', alpha=0.5)
 
-    person_df_filtered = person_df[person_df['relative_path'].isin(IMAGE_PATHS)]
-
-    for idx, row in person_df_filtered.iterrows():
-        rel_path = row['relative_path']
-        arg_label = row['arg_label']
-        arg_box = json.loads(row['arg_bounding_box'])
-        ref_label = row['ref_label']
-        ref_box = json.loads(row['ref_bounding_box'])
-        img_path = ROOT_DIR + rel_path
-        orig_img = cv2.imread(img_path, cv2.IMREAD_ANYCOLOR)
-        img_width = orig_img.shape[1]
-        img_height = orig_img.shape[0]
-        matlab_args = {'img_width': img_width,
-                       'img_height': img_height,
-                       'ref_bounding_box': ref_box,
-                       'arg_bounding_box': arg_box
-                       }
-        f0, f2, hyb = compute_hof_display(eng, matlab_args)
-
-        # Plot the resultant histograms
-        hist_file = HIST_OUTPUT_DIR + arg_label + '_' + ref_label
-        plt.bar(np.arange(361), f0)
-        plt.xticks(np.arange(0, 361, 45))
-        plt.savefig(hist_file + '_f0.png')
-        plt.show()
-
-        plt.bar(np.arange(361), f2)
-        plt.xticks(np.arange(0, 361, 45))
-        plt.savefig(hist_file + '_f2.png')
-        plt.show()
-
-        plt.bar(np.arange(361), hyb)
-        plt.xticks(np.arange(0, 361, 45))
-        plt.savefig(hist_file + '_hyb.png')
-        plt.show()
-
-        print('Finished creating histogram. Drawing Mask Image')
-
-        blank_img = np.zeros((img_height, img_width, 3), np.uint8)
-        cv2.rectangle(blank_img, (arg_box[0], arg_box[1]), (arg_box[2], arg_box[3]), (255, 255, 255), -1)
-        cv2.rectangle(blank_img, (ref_box[0], ref_box[1]), (ref_box[2], ref_box[3]), (128, 128, 128), -1)
-        img_file = hist_file + '_bounding_boxes.jpg'
-        cv2.imwrite(img_file, blank_img)
+    ax.bar(np.arange(361), magnitude, color='blue')
+    ax.set_xticks(np.arange(0, 361, 45))
+    plt.legend(loc='upper left')
+    plt.xlabel('Degrees')
+    plt.ylabel('Magnitude')
+    plt.savefig(IMG_OUTPUT_FILE)
+    plt.show()
 
 
 if __name__ == '__main__':
